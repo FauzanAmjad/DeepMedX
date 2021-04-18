1430b91d098da8a818e9e394e05372593bf044d8
Rahul Hegde <rhegde.hrahul@gmail.com>
2021-04-18 12:14:34 -0400

    some more changes

--- DeepMedX/App.js
+++ DeepMedX/App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
